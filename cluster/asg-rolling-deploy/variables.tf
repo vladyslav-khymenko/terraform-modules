869a363bdab3c4243cf2d18c67a0579a9c557d5d
vladyslav-khymenko <vkhymenko@protonmail.com>
2023-05-20 16:51:03 +0300

    added validation to input variable

--- cluster/asg-rolling-deploy/variables.tf
+++ cluster/asg-rolling-deploy/variables.tf
@@ -11,6 +11,11 @@ variable "ami" {
 variable "instance_type" {
   description = "The type of EC2 instances to run (e.g. t2.micro)"
   type        = string
+
+  validation {
+    condition = contains(["t2.micro", "t3.micro"], var.instance_type)
+    error_message = "Only free tier is allowed: t2.micro | t3.micro"
+  }
 }
 
 variable "min_size" {
